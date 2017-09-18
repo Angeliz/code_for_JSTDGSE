259c428be71de569c595e16a8df54766da784428
Angeliz <471330418@qq.com>
2017-09-18 22:11:36 +0800

    Math

---
+++ chapter2.js
@@ -0,0 +1,18 @@
+//Math
+Math.pow(2,53)              //2的53次幂
+Math.round(0.6)				//四舍五入
+Math.ciel(0.6)				//向上取整
+Math.floor(0.6)				//向下取整
+Math.abs(-5)				//绝对值
+Math.max(x,y,z)				
+Math.min(x,y,z)
+Math.random()				//生成一个大于等于0小于1的伪随机数
+Math.PI						//π
+Math.exp   					//e
+Math.sqrt(3)				//平方根
+Math.pow(3,1/3)				//立方根
+Math.sin(0)	
+Math.log(10)				//10的自然对数
+Math.log(100)/Math.LN10		//以10为底100的对数
+Math.log(512)/Math.LN2		//以2为底512的对数
+Math.exp(3)					//e的三次幂
